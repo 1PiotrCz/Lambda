90bc1590f9146f55d4b956f6d73287fea050846d
Piotr Czubkowski <piotrcz@o2.pl>
2017-05-22 20:47:18 +0200

    add Ex3, sorter and Consumer.

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -14,9 +14,18 @@ public class Calculator {
         };
 
         IntegerMath subtraction = (a,b) -> a-b;
+        IntegerMath multiplication = (a,b) -> a*b;
+        IntegerMath division = (a,b) -> {
+            if (b == 0) {
+                throw new IllegalArgumentException("Nie dziel przez zero");
+            }
+            return a/b;
+        };
 
         System.out.println(operateBinary(5,10,adding));
         System.out.println(operateBinary(20,10,subtraction));
+        System.out.println(operateBinary(20,10,multiplication));
+        System.out.println(operateBinary(20,0,division));
     }
 
     public static int operateBinary (int a, int b, IntegerMath math){
---
+++ src/com/company/Ex3.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Created by Piotr Czubkowski on 2017-05-22.
+ */
+public class Ex3 {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Piotrek", "Ania", "Monika");
+
+        Collections.sort(names, (arg1, arg2) -> arg1.compareToIgnoreCase(arg2));
+        for (String name : names) {
+            System.out.println(name);
+        }
+//        Collections.sort(names, new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return o1.compareToIgnoreCase(o2);
+//            }
+//        });
+//        for (String name : names) {
+//
+//        }
+        Consumer consumer = arg1 -> System.out.println(arg1)
+//        names.forEach(arg1 -> System.out.println(arg1));
+        names.forEach(consumer);
+    }
+}
